2d1e06b6c73e1a12a8259c89c2a362201dc232e9
MASA20201203 <koedame032@gmail.com>
2020-12-17 10:43:54 +0900

    5-2-2回答

--- introduction_sql/practice5.sql
+++ introduction_sql/practice5.sql
@@ -9,5 +9,19 @@ UPDATE 回答者
                                       WHEN 'vn' THEN 'ベトナム'
       END
 
--- (1)
+-- (2)
+SELECT TRIM(メールアドレス) AS メールアドレス, 
+       CONCAT(CASE WHEN 年齢 >= 20 AND 年齢 <=29 THEN "20代:"
+                   WHEN 年齢 >= 30 AND 年齢 <=39 THEN "30代:" 
+                   WHEN 年齢 >= 40 AND 年齢 <=49 THEN "40代:" 
+                   WHEN 年齢 >= 50 AND 年齢 <=59 THEN "50代:" 
+              END,
+              CASE 性別 WHEN 'M' THEN '男性'
+                        WHEN 'F' THEN '女性'
+              END
+              ) AS 属性
+  FROM 回答者
+
+-- 問題 5-3
+
 
