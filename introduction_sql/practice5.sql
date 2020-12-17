170e822ca6e76af581c92354c7dd565f9062bff0
MASA20201203 <koedame032@gmail.com>
2020-12-17 12:34:26 +0900

    5-3-2回答

--- introduction_sql/practice5.sql
+++ introduction_sql/practice5.sql
@@ -30,5 +30,19 @@ UPDATE 受注
    SET 文字数 = LENGTH(REPRACE(文字, '　', ' '))
 
 --(2)
-SELECT 受注日, 
+SELECT 受注日, 受注ID, 文字数, 
+        CASE 書体コード WHEN '1' OR NULL THEN 'ブロック体'
+                       WHEN '2' THEN '筆記体'
+                       WHEN '3' THEN '草書体'
+        END AS 書体名,
+        文字数 * 
+          CASE 書体コード WHEN '1' OR NULL THEN 100
+                        WHEN '2' THEN 150
+                        WHEN '3' THEN 200
+          END AS 単価,
+        CASE WHEN 文字数 >= 10 THEN 500
+             ELSE 0
+        END AS 特別加工量
+  FROM 受注
+ORDER BY 受注日, 受注ID
 
