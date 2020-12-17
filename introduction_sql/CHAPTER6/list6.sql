5b2bdab815e261bbb175b1f0ff3ba2b89868d863
MASA20201203 <koedame032@gmail.com>
2020-12-17 14:34:47 +0900

    6-7 費目でグループ化してそれぞれの合計を求める

--- introduction_sql/CHAPTER6/list6.sql
+++ introduction_sql/CHAPTER6/list6.sql
@@ -22,3 +22,8 @@ SELECT 日付け, SUM(出金額) AS 出金額合計
 --6-5 NULLをゼロとして平均を求める
 SELECT AVG(COALESCE(出金額, 0)) AS 出金額の平均
   FROM 家計簿
+
+--6-7 費目でグループ化してそれぞれの合計を求める
+SELECT 費目, SUM(出金額) AS 費目別の出金額合計
+  FROM 家計簿
+ GROUP BY 費目
