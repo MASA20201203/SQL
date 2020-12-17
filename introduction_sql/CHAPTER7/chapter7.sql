5529e43ef53b93e5505c4617d988d58e1ba1ed63
MASA20201203 <koedame032@gmail.com>
2020-12-18 06:48:02 +0900

    7-7 ANYで副問い合わせを利用する

--- introduction_sql/CHAPTER7/chapter7.sql
+++ introduction_sql/CHAPTER7/chapter7.sql
@@ -20,3 +20,9 @@ SELECT 日付, メモ, 出金額,
 -- 7-6 INで副問い合わせを利用する
 SELECT * FROM 家計簿集計
  WHERE 費目 IN (SELECT DISTINCT 費目 FROM 家計簿)
+
+-- 7-7 ANYで副問い合わせを利用する
+SELECT * FROM 家計簿
+ WHERE 費目 = '食費'
+   AND 出金額 < ANY (SELECT 出金額 FROM 家計簿アーカイブ
+                      WHERE 費目 = '食費')
