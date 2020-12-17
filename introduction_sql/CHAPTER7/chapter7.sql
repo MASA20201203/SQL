bd0d1ad505de8045d9ea64d7b576538944ecd56f
MASA20201203 <koedame032@gmail.com>
2020-12-18 07:16:46 +0900

    7-11 FROM句で服問い合わせを利用する

--- introduction_sql/CHAPTER7/chapter7.sql
+++ introduction_sql/CHAPTER7/chapter7.sql
@@ -26,3 +26,22 @@ SELECT * FROM 家計簿
  WHERE 費目 = '食費'
    AND 出金額 < ANY (SELECT 出金額 FROM 家計簿アーカイブ
                       WHERE 費目 = '食費')
+
+-- 7-9 副問い合わせからNULLを除外する(IS NOT NULL)
+SELECT * FROM 家計簿アーカイブ
+ WHERE 費目 IN ( SELECT 費目 FROM 家計簿
+                  WHERE 費目 IS NOT NULL)
+
+-- 7-10 副問い合わせからNULLを除外する(COALESCE)
+SELECT * FROM 家計簿アーカイブ
+ WHERE 費目 IN ( SELECT COALESCE(費目, '不明') FROM 家計簿)
+
+-- 7-11 FROM句で服問い合わせを利用する
+SELECT SUM(SUB.出金額) AS 出金額合計
+  FROM (SELECT 日付, 費目, 出金額
+          FROM 家計簿
+         UNION
+        SELECT 日付, 費目, 出金額
+          FROM 家計簿アーカイブ
+         WHERE 日付 >= '2018-01-01'
+           AND 日付 <= '2018-01-31') AS SUB
