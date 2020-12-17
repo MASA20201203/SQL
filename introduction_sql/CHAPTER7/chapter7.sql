c211cd2d38fc9995c7e4a70eb0c31d037563b99d
MASA20201203 <koedame032@gmail.com>
2020-12-17 15:56:11 +0900

    7-4 選択リストで副問い合わせを利用する

--- introduction_sql/CHAPTER7/chapter7.sql
+++ introduction_sql/CHAPTER7/chapter7.sql
@@ -9,3 +9,10 @@ UPDATE 家計簿集計
                 WHERE 出金額 > 0
                   AND 費目 = '食費')
  WHERE 費目 = '食費'
+
+-- 7-4 選択リストで副問い合わせを利用する
+SELECT 日付, メモ, 出金額,
+        (SELECT 合計 FROM 家計簿集計
+          WHERE 費目 = '食費') AS 過去の合計額
+  FROM 家計簿アーカイブ
+ WHERE 費目 = '食費'
