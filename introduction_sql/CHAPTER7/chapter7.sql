29d13582dd8cdf3a8bf9bee733c5e494cf424898
MASA20201203 <koedame032@gmail.com>
2020-12-18 06:34:18 +0900

    7-6 INで副問い合わせを利用する

--- introduction_sql/CHAPTER7/chapter7.sql
+++ introduction_sql/CHAPTER7/chapter7.sql
@@ -16,3 +16,7 @@ SELECT 日付, メモ, 出金額,
           WHERE 費目 = '食費') AS 過去の合計額
   FROM 家計簿アーカイブ
  WHERE 費目 = '食費'
+
+-- 7-6 INで副問い合わせを利用する
+SELECT * FROM 家計簿集計
+ WHERE 費目 IN (SELECT DISTINCT 費目 FROM 家計簿)
