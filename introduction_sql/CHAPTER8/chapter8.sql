2ab951b63aeaf7adc1fbdaeda3f8f4e491b6dc6e
MASA20201203 <koedame032@gmail.com>
2020-12-18 09:42:09 +0900

    8-8 3つのテーブルを結合するSQL文

--- introduction_sql/CHAPTER8/chapter8.sql
+++ introduction_sql/CHAPTER8/chapter8.sql
@@ -15,3 +15,11 @@ SELECT 日付, K.メモ, H.メモ
   FROM 家計簿 AS K
   JOIN 費目 AS H
     ON K.費目ID = H.ID
+
+-- 8-8 3つのテーブルを結合するSQL文
+SELECT 日付, 費目.名前, 経費区分.名称
+  FROM 家計簿
+  JOIN 費目
+    ON 家計簿.費目ID = 費目.ID
+  JOIN 経費区分
+    ON 費目.経費区分ID = 経費区分.ID
