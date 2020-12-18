f5ed02339a5a7180b4b0cf0c584215abe2fbaaef
MASA20201203 <koedame032@gmail.com>
2020-12-18 14:39:04 +0900

    10-2 家計簿テーブルに対する行の追加

--- introduction_sql/CHAPTER10/chapter10.sql
+++ introduction_sql/CHAPTER10/chapter10.sql
@@ -7,4 +7,16 @@ CREATE TABLE 家計簿 (
   出金額    INTEGER
 )
 
--- 10-2 家計簿
+-- 10-2 家計簿テーブルに対する行の追加
+INSERT INTO 家計簿 ( 日付, メモ, 出金額)
+VALUES ('2018-04-12', '詳細は後で', 60000)
+
+-- 10-3 家計簿テーブルを作成する
+CREATE TABLE 家計簿 (
+  日付      DATE,
+  費目ID    INTEGER,
+  メモ      VARCHAR(100),
+  入金額    INTEGER,
+  出金額    INTEGER
+)
+
