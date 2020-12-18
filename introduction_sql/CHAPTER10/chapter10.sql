7e0ff10bbce9c70a2c3adbbfa3d0c20c66b50a6b
MASA20201203 <koedame032@gmail.com>
2020-12-18 15:23:15 +0900

    10-10 外部キー制約の指定

--- introduction_sql/CHAPTER10/chapter10.sql
+++ introduction_sql/CHAPTER10/chapter10.sql
@@ -61,7 +61,14 @@ CREATE TABLE 費目 (
   PRIMARY KEY(ID, 名前)
 );
 
-
+-- 10-10 外部キー制約の指定
+CREATE TABLE 家計簿 (
+  日付      DATE          NOT NULL,
+  費目ID    INTEGER       REFERENCES 費目(ID),
+  メモ      VARCHAR(100)  DEFAULT '不明' NOT NULL,
+  入金額    INTEGER       DEFAULT 0 CHECK (入金額 >= 0),
+  出金額    INTEGER       DEFAULT 0 CHECK (出金額 >= 0)
+);
 
 
 
